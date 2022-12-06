ac83402c3a1e4181cc8de9a77b11d3f2e208cd7c
Ian Wootten <3598161+iwootten@users.noreply.github.com>
2022-12-06 17:06:50 +0000

    Correct test names

--- 2022/tests/test_day5.py
+++ 2022/tests/test_day5.py
@@ -1,13 +1,15 @@
 from day5 import get_data, get_part1_answer, get_part2_answer
 
 
-def test_day1_part1():
+def test_day5_part1():
     data = get_data("./data/day5_example.txt")
     answer = get_part1_answer(data)
     assert answer == "CMZ"
 
 
-def test_day1_part2():
+def test_day5_part2():
     data = get_data("./data/day5_example.txt")
     answer = get_part2_answer(data)
     assert answer == "MCD"
+
+
